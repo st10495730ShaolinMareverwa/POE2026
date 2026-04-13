02513a245e90aa144be1cff7b8864112d0a5088a
st10495730ShaolinMareverwa <st10495730@myemeris.edu.za>
2026-04-13 21:03:51 +0200

    Third Commit

--- src/poe1/POE1.java
+++ src/poe1/POE1.java
@@ -1,15 +1,48 @@
-
+    
 package poe1;
 
+import java.util.Scanner;
 public class POE1 
 {
 
     
     public static void main(String[] args) 
     {
-        
-    }     
-            
-}
       
-    
+        // Scanner is used to read input from the keyboard
+        Scanner input = new Scanner(System.in);
+
+        // Create a Login object to store details and use the validation methods
+        Login login = new Login();
+
+        // Registration 'heading'
+        System.out.println("=== Registration ===");
+
+        // Ask user for their first name
+        System.out.print("Enter first name: ");
+        login.setFirstName(input.nextLine()); 
+
+        // Ask user for their last name
+        System.out.print("Enter last name: ");
+        login.setLastName(input.nextLine());
+
+        // Ask user for their preferred username
+        System.out.print("Enter username: ");
+        login.setUserName(input.nextLine());
+
+        // Check inputted username and show the appropriate message
+        if (login.checkUserName()) 
+        {
+            System.out.println("Username successfully captured.");
+        } else 
+        {
+            System.out.println("Username is not correctly formatted; please "
+                    + "ensure that your username contains an underscore and is "
+                    + "no more than five characters in length.");
+        }
+
+        // Ask user for password
+        System.out.print("Enter password: ");
+        login.setPassword(input.nextLine());
+        
+        
